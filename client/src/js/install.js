f46024d720dd24ded75fd2d37aa8035d25683776
Michael Andrews <michael.andrews@pega.com>
2022-10-05 11:27:53 -0400

    updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,8 +4,7 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-
-    installBtn.classList.toggle('hidden', false);
+    butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
